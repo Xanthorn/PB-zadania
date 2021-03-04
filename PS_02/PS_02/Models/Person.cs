2d032b70f40009444516fb42b3185ef6442acc7e
Piotr Szymoniak <szymoniakpl@gmail.com>
2021-03-04 18:53:13 +0100

    Add Person Class

---
+++ PS_02/PS_02/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOT_NET_PS_02.Models
+{
+    public enum Gender
+    {
+
+    }
+    public class Person
+    {
+        [Display(Name = "Imię")]
+        [Required(ErrorMessage = "Pole 'Imię' jest obowiązkowe")]
+        public string Name;
+        [Display(Name = "E-mail")]
+        public string Email;
+        [Display(Name = "Wzrost")]
+        [Required(ErrorMessage = "Pole 'Wzrost' jest obowiązkowe")]
+        public double Height;
+        [Display(Name = "Waga")]
+        [Required(ErrorMessage = "Pole 'Waga' jest obowiązkowe")]
+        public double Weight;
+        [Display(Name = "Wiek")]
+        public double Age;
+        [Display(Name = "Płeć")]
+        [Required(ErrorMessage = "Pole 'Płeć' jest obowiązkowe")]
+        
+
+    }
+}
