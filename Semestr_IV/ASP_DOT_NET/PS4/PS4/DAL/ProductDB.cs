8d44edd573f53d9cb2f5410709d749779d3cc2b3
Piotr Szymoniak <59312494+Xanthorn@users.noreply.github.com>
2021-03-21 16:55:21 +0100

    Add ProductDB class

---
+++ Semestr_IV/ASP_DOT_NET/PS4/PS4/DAL/ProductDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PS4.Models;
+using System.Text.Json;
+
+namespace PS4.DAL
+{
+    public class ProductDB
+    {
+        private List<Product> products;
+        public void Load(string jsonProducts)
+        {
+            if (jsonProducts == null)
+            {
+                products = Product.GetProducts();
+            }
+            else
+            {
+                // products = JsonConvert.DeserializeObject<List<Product>>(jsonProducts);
+                products = JsonSerializer.Deserialize<List<Product>>(jsonProducts);
+            }
+        }
+        private int GetNextId()
+        {
+            int lastID = products[products.Count - 1].id;
+            int newID = lastID++;
+            return newID;
+        }
+        public void Create(Product p)
+        {
+            p.id = GetNextId();
+            products.Add(p);
+        }
+        public string Save()
+        {
+
+            //return JsonConvert.SerializeObject(products);
+            return JsonSerializer.Serialize(products);
+        }
+        public List<Product> List()
+        {
+            return products;
+        }
+    }
+}
+
