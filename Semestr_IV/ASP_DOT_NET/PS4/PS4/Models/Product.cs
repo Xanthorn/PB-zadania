4cfe135c6f3fd1b5022184407f0d165288d976b0
Piotr Szymoniak <59312494+Xanthorn@users.noreply.github.com>
2021-03-21 16:27:40 +0100

    Add Product class

---
+++ Semestr_IV/ASP_DOT_NET/PS4/PS4/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PS4.Models
+{
+    public class Product
+    {
+        [Display(Name = "Id")] 
+        public int id { get; set; }
+        [Display(Name = "Nazwa")] 
+        public string name { get; set; }
+        [Display(Name = "Cena")] 
+        public decimal price { get; set; }
+        public static List<Product> GetProducts() 
+        { 
+            Product pilka = new Product { id = 1, name = "Piłka nożna", price = 25.30M }; 
+            Product narty = new Product { id = 2, name = "Narty", price = 150.39M }; 
+            Product rakieta = new Product { id = 3,name = "Rakieta ", price = 111.10M}; 
+            return new List<Product> { pilka, narty, rakieta }; }
+        }
+}
