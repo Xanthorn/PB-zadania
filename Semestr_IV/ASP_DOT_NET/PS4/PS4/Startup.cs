85efc770cc870eaa90ee39a0453c7b7959025ee3
Piotr Szymoniak <59312494+Xanthorn@users.noreply.github.com>
2021-03-21 15:13:26 +0100

    Modify Startup.cs

--- Semestr_IV/ASP_DOT_NET/PS4/PS4/Startup.cs
+++ Semestr_IV/ASP_DOT_NET/PS4/PS4/Startup.cs
@@ -23,6 +23,9 @@ namespace PS4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddSessionStateTempDataProvider(); 
+            services.AddSession();
+
             services.AddRazorPages();
         }
 
@@ -43,6 +46,9 @@ namespace PS4
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            // --sesja
+            app.UseSession();
+            // --
             app.UseRouting();
 
             app.UseAuthorization();
