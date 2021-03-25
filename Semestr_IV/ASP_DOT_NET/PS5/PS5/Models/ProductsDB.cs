51b0912020786047dca195a5e16308b2435516a0
Piotr Szymoniak <59312494+Xanthorn@users.noreply.github.com>
2021-03-25 14:47:59 +0100

    Simplify AddProduct and GetProducts method Add DeleteProduct method

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Models/ProductsDB.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Models/ProductsDB.cs
@@ -10,40 +10,56 @@ namespace PS5.Models
 {
     public class ProductsDB
     {
-        public static List<Product> GetProducts(List<Product> products, IConfiguration _configuration)
+        public static List<Product> GetProducts(List<Product> products, IConfiguration configuration)
         {
             products = new List<Product>();
 
-            string ConnectionString = _configuration.GetConnectionString("PS5DB");
+            string ConnectionString = configuration.GetConnectionString("PS5DB");
 
-            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlConnection con = new(ConnectionString);
             string sql = "SELECT * FROM Products";
-            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlCommand cmd = new(sql, con);
             con.Open();
             SqlDataReader reader = cmd.ExecuteReader();
             products = new List<Product>();
             while (reader.Read())
             {
-                Product product = new Product();
-                product.Id = Int32.Parse(reader["Id"].ToString());
-                product.Name = reader["Name"].ToString();
-                product.Price = Double.Parse(reader["Price"].ToString());
+                Product product = new()
+                {
+                    Id = Int32.Parse(reader["Id"].ToString()),
+                    Name = reader["Name"].ToString(),
+                    Price = Double.Parse(reader["Price"].ToString())
+                };
                 products.Add(product);
             }
             reader.Close(); con.Close();
             return products;
         }
-        public static void AddProduct(Product product, IConfiguration _configuration)
+        public static void AddProduct(Product product, IConfiguration configuration)
         {
             String query = @$"INSERT INTO dbo.Products (Name, Price, Description) VALUES (@Name, @Price, @Description);";
-            string connectionString = _configuration.GetConnectionString("PS5DB");
+            string connectionString = configuration.GetConnectionString("PS5DB");
 
-            using(SqlConnection cn = new SqlConnection(connectionString))
-            using (SqlCommand cmd = new SqlCommand(query, cn))
+            using(SqlConnection cn = new(connectionString))
+            using (SqlCommand cmd = new(query, cn))
             {
                 cmd.Parameters.Add("@Name", SqlDbType.VarChar, 50).Value = product.Name;
                 cmd.Parameters.Add("@Price", SqlDbType.Decimal, 18).Value = product.Price;
                 cmd.Parameters.Add("@Description", SqlDbType.VarChar, 500).Value = product.Description;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+        }
+        public static void DeleteProduct(int id, IConfiguration configuration)
+        {
+            String query = @$"DELETE FROM dbo.Products WHERE Id = @id";
+            string connectionString = configuration.GetConnectionString("PS5DB");
+
+            using (SqlConnection cn = new(connectionString))
+            using (SqlCommand cmd = new(query, cn))
+            {
+                cmd.Parameters.Add("@Id", SqlDbType.VarChar, 50).Value = id;
                 cn.Open();
                 cmd.ExecuteNonQuery();
                 cn.Close();
