49be86110b98ea4ca7a2a898fc18797914bd80e2
Piotr Szymoniak <59312494+Xanthorn@users.noreply.github.com>
2021-03-25 13:57:12 +0100

    Add Description Value for Insert in AddElement()

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Models/ProductsDB.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Models/ProductsDB.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace PS5.Models
 {
@@ -31,6 +32,22 @@ namespace PS5.Models
             }
             reader.Close(); con.Close();
             return products;
+        }
+        public static void AddProduct(Product product, IConfiguration _configuration)
+        {
+            String query = @$"INSERT INTO dbo.Products (Name, Price, Description) VALUES (@Name, @Price, @Description);";
+            string connectionString = _configuration.GetConnectionString("PS5DB");
+
+            using(SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                cmd.Parameters.Add("@Name", SqlDbType.VarChar, 50).Value = product.Name;
+                cmd.Parameters.Add("@Price", SqlDbType.Decimal, 18).Value = product.Price;
+                cmd.Parameters.Add("@Description", SqlDbType.VarChar, 500).Value = product.Description;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
         }
     }
 }
