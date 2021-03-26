8c121df0bd68e36c7de8a330517fdb0735ac0ccd
Piotr <Piotr@DESKTOP-Amelii>
2021-03-26 22:02:08 +0100

    Add fine working Remove page, but without cookies update

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Remove.cshtml.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Remove.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using PS5.Models;
 
 namespace PS5.Pages
 {
     public class RemoveModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Product Product { get; set; }
+        private readonly IConfiguration _configuration;
+        public RemoveModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+        public void OnGet(int id)
+        {
+            Product = ProductsDB.GetProduct(id, _configuration);
+        }
+        public IActionResult OnPost()
+        {
+            int option = Int32.Parse(Request.Form["Option"]);
+            if(option == 1)
+            {
+                ProductsDB.RemoveProduct(Product.Id, _configuration);
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                return RedirectToPage("/Index");
+            }
         }
     }
 }
