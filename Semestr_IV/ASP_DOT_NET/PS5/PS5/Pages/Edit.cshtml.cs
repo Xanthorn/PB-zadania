22f553a809049f66880a46c6e36c81747df42917
Piotr <Piotr@DESKTOP-Amelii>
2021-03-26 22:04:25 +0100

    Revert "Revert "Add RemoveProduct and GetProduct method to ProductDB class""
    This reverts commit bfc0c9b854cca75fe25d00cc8d9ace1863d2a08a.

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Edit.cshtml.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Edit.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using PS5.Models;
 
 namespace PS5.Pages
 {
     public class EditModel : PageModel
     {
+        [BindProperty]
+        public Product EditProduct { get; set; }
+        private readonly IConfiguration _configuration;
+        public EditModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void OnGet()
         {
+
         }
     }
 }
