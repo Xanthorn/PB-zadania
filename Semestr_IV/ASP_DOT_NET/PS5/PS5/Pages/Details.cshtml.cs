0a795b466dee9a76d57276e21f1efb94babf0fa5
Piotr <Piotr@DESKTOP-Amelii>
2021-03-27 14:55:58 +0100

    Add: Add Product to shopping cart action

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Details.cshtml.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Details.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,19 @@ namespace PS5.Pages.Shared
         public void OnGet(int id)
         {
             Product = ProductsDB.GetProduct(id, _configuration);
+        }
+        public IActionResult OnPost()
+        {
+            int click = Int32.Parse(Request.Form["click"]);
+            if(click == 1)
+            {
+                Product = ProductsDB.GetProduct(Product.Id, _configuration);
+                string currentShoppingCart = Request.Cookies["ShoppingCart"];
+                string newShoppingCart = ShoppingCartCookie.AddProduct(Product, currentShoppingCart);
+                var cookieOptions = new CookieOptions { Expires = DateTime.Now.AddDays(1) };
+                Response.Cookies.Append("ShoppingCart", newShoppingCart, cookieOptions);
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
