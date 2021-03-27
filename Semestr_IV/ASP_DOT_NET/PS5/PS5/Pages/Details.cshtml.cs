3d7135e2eb8ab9c200185b590ed6d44dd66aa140
Piotr <Piotr@DESKTOP-Amelii>
2021-03-27 13:25:01 +0100

    Add Details Razor Page

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Details.cshtml.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Details.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using PS5.Models;
 
 namespace PS5.Pages.Shared
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Product Product { get; set; }
+        private readonly IConfiguration _configuration;
+        public DetailsModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+        public void OnGet(int id)
+        {
+            Product = ProductsDB.GetProduct(id, _configuration);
         }
     }
 }
