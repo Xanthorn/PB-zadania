fd82842b580ada94261e00931ae80cf3e8e129d4
Piotr <Piotr@DESKTOP-Amelii>
2021-03-27 14:57:28 +0100

    Add: Delete product from cookie when delete from dbo.Products

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Remove.cshtml.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Remove.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -27,7 +28,12 @@ namespace PS5.Pages
             int click = Int32.Parse(Request.Form["click"]);
             if (click == 1)
             {
+                Product = ProductsDB.GetProduct(Product.Id, _configuration);
                 ProductsDB.RemoveProduct(Product.Id, _configuration);
+                string currentShoppingCart = Request.Cookies["ShoppingCart"];
+                string newShoppingCart = ShoppingCartCookie.RemoveProduct(currentShoppingCart, Product);
+                var cookieOptions = new CookieOptions { Expires = DateTime.Now.AddDays(1) };
+                Response.Cookies.Append("ShoppingCart", newShoppingCart, cookieOptions);
                 return RedirectToPage("/Index");
             }
             else
