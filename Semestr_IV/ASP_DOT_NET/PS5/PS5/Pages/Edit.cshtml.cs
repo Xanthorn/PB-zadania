c69743bda84cea389d334ce89935eafd8f87650c
Piotr <Piotr@DESKTOP-Amelii>
2021-03-27 14:56:49 +0100

    Add: Modify existing shopping cart cookie after dbo.Products modify

--- Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Edit.cshtml.cs
+++ Semestr_IV/ASP_DOT_NET/PS5/PS5/Pages/Edit.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -35,6 +36,10 @@ namespace PS5.Pages
             if (click == 1)
             {
                 ProductsDB.UpdateProduct(NewProduct, _configuration);
+                string currentShoppingCart = Request.Cookies["ShoppingCart"];
+                string newShoppingCart = ShoppingCartCookie.UpdateProduct(currentShoppingCart, NewProduct);
+                var cookieOptions = new CookieOptions { Expires = DateTime.Now.AddDays(1) };
+                Response.Cookies.Append("ShoppingCart", newShoppingCart, cookieOptions);
                 return RedirectToPage("/Index");
             }
             else
