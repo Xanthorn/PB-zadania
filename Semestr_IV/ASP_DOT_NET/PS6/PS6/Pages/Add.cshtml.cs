18ac71f8fb7f3a76dcfd21294481340da308d246
Piotr Szymoniak <59312494+Xanthorn@users.noreply.github.com>
2021-04-11 17:15:32 +0200

    some changes i forgot about pushing brand new project

---
+++ Semestr_IV/ASP_DOT_NET/PS6/PS6/Pages/Add.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using PS6.Models;
+
+namespace PS6.Pages
+{
+    public class AddModel : PageModel
+    {
+        [BindProperty]
+        public Product NewProduct { get; set; }
+        private readonly IConfiguration _configuration;
+        public AddModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void OnGet()
+        {
+            NewProduct = new();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                ProductsDB.AddProduct(NewProduct, _configuration);
+                return RedirectToPage("/Index");
+            }
+        }
+    }
+}
---
+++ Semestr_IV/ASP_DOT_NET/PS6/PS6/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PS6.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace PS6.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public List<Product> Products { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductsDB.GetProducts(Products, _configuration);
+        }
+    }
+}
