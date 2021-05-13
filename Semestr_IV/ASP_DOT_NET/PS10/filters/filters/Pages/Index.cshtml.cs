e886a4fc3a60068d48c3e8c4b7be31e7f78a52fe
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-05-13 16:37:23 +0200

    final version

---
+++ Semestr_IV/ASP_DOT_NET/PS10/filters/filters/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using filters.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+namespace filters.Pages
+{
+    [CustomFilterAttributes]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public int testVriable { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+            int a = 0;
+        }
+    }
+}
---
+++ Semestr_IV/ASP_DOT_NET/PS10/filters/filters/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filters.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["filterMessage"] = "Januszex";
+            }
+        }
+    }
+}
