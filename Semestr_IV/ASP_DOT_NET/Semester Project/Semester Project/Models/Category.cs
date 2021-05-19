629a1fb9bce0ec0595d3e14fc682db8d3f978613
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-05-19 23:45:46 +0200

    add validation to basic classes

--- Semestr_IV/ASP_DOT_NET/Semester Project/Semester Project/Models/Category.cs
+++ Semestr_IV/ASP_DOT_NET/Semester Project/Semester Project/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Semester_Project.Models
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        [MaxLength(50, ErrorMessage = "Maksymalna długość nazwy nie powinna być dłuższa niż 50 znaków")]
+        [MinLength(5, ErrorMessage = "Minimalna długość nazwy nie powinna być krótsza niż 5 znaków")]
         public string Name { get; set; }
     }
 }
